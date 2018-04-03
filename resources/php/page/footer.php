d7152493ca0bab4005f34bd10f21ca793d743f3e
wjbaker101 <willbaker10198@gmail.com>
2018-04-03 20:02:20 +0100

    Remove footer classes

--- resources/php/page/footer.php
+++ resources/php/page/footer.php
@@ -1,4 +1,4 @@
-<footer class="bg-pure-white padding-small">
+<footer>
     <div class="content-width">
         <p>
             Copyright &copy; 2015&mdash;<?= (new DateTime())->format('Y') ?> William Baker.<br>
