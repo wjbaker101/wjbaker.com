c41fe588b666a954052009d2aca6abb23b311026
wjbaker101 <willbaker10198@gmail.com>
2018-04-05 21:47:37 +0100

    Embed raw icon files

--- resources/php/page/nav.php
+++ resources/php/page/nav.php
@@ -1,8 +1,20 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/resources/php/icons/icons.php'); ?>
+
 <nav class="bg-theme">
-    <a class="home-button" href="/">=</a>
+    <a class="home-button" href="/" title="Return to Homepage">
+        <?= Icons::home() ?>
+    </a>
     <ul class="links">
-        <li><a href="/projects/">Projects</a></li>
-        <li><a href="/blog/">Blog</a></li>
-        <li><a href="/user/login/">User</a></li>
+        <li>
+            <a href="/projects/"><?= Icons::projects() ?>Projects</a>
+        </li>
+        <li>
+            <a href="/blog/"><?= Icons::blog() ?>Blog</a>
+        </li>
+        <li>
+            <a href="/user/login/"><?= Icons::user() ?>User</a>
+        </li>
     </ul>
+
+    <!-- <div>Icons made by <a href="https://www.flaticon.com/authors/anton-saputro" title="Anton Saputro">Anton Saputro</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> -->
 </nav>
