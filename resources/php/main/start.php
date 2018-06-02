7f141c20cf88bfcac1e1cb4ce1e408377ee231d3
wjbaker101 <willbaker10198@gmail.com>
2018-06-02 12:32:14 +0100

    Create template include functions

--- resources/php/main/start.php
+++ resources/php/main/start.php
@@ -1,25 +1,12 @@
 <?php
 
+require_once($_SERVER['DOCUMENT_ROOT'] . '/resources/php/page/page.php');
+
 start();
 
 function start()
 {
-    definePageLayout();
     
-    define('LINK_REL', 'noopener noreferrer');
-}
-
-function definePageLayout()
-{
-    define('PAGE_META', $_SERVER['DOCUMENT_ROOT'] . '/resources/php/page/meta.php');
-
-    define('PAGE_STYLE', $_SERVER['DOCUMENT_ROOT'] . '/resources/php/page/style.php');
-
-    define('PAGE_SCRIPTS', $_SERVER['DOCUMENT_ROOT'] . '/resources/php/page/scripts.php');
-
-    define('PAGE_NAV', $_SERVER['DOCUMENT_ROOT'] . '/resources/php/page/nav.php');
-
-    define('PAGE_FOOTER', $_SERVER['DOCUMENT_ROOT'] . '/resources/php/page/footer.php');
 }
 
 ?>
