f256b844c83dada1e9c4ed9b0d3489408554ce98
wjbaker101 <willbaker10198@gmail.com>
2019-10-24 22:23:47 +0100

    Update CV

--- src/view/AboutCVView.vue
+++ src/view/AboutCVView.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="page-content cv-content">
         <h2 class="text-centered">William Baker</h2>
+
+        <!-- START SECTION profile -->
+
         <h3 class="cv-heading">Profile</h3>
         <div class="cv-row-half">
             <div>
@@ -40,10 +43,27 @@
                 </div>
             </div>
         </div>
+
+        <!-- END SECTION profile -->
+        <!-- START SECTION education -->
+
         <h3 class="cv-heading">Education</h3>
         <div class="cv-row-heading">
-            <div>BSci Computer Science, Loughborough University</div>
+            <div>BSci Computer Science with Placement, Loughborough University (Third Year)</div>
             <div>2016 &ndash; Present</div>
+        </div>
+        <div class="cv-row">
+            <div><strong>2nd Year Marks:</strong></div>
+            <div>Average: 76.2%</div>
+        </div>
+        <div class="cv-row">
+            <div><strong>Relevant Modules:</strong></div>
+            <div>
+                Mobile Application Development: 91%,<br>
+                Operating Systems, Networks and the Internet 1: 85%,<br>
+                Software Engineering 2: 81%,<br>
+                Operating Systems, Networks and the Internet 2: 79%
+            </div>
         </div>
         <div class="cv-row">
             <div><strong>1st Year Marks:</strong></div>
@@ -77,61 +97,59 @@
             <div><strong>GCSEs:</strong></div>
             <div>3 A*, 5 A, 3 B, (A*s in Mathematics, Computing and Physics)</div>
         </div>
+
+        <!-- END SECTION education -->
+        <!-- START SECTION work -->
+
         <h3 class="cv-heading">Work Experience</h3>
+        <div class="cv-row-heading">
+            <div>Yell - Undergraduate Software Developer</div>
+            <div>July 2018 – August 2019</div>
+        </div>
+        <ul>
+            <li>Maintained multiple cloud-hosted Java APIs, using frameworks such as Spring Boot.</li>
+            <li>Debugged remote-hosted Docker systems.</li>
+            <li>Scrum, pair-programming and group project based development.</li>
+            <li>Developed using both continuous integration and continuous delivery methodologies.</li>
+            <li>Code quality control using SonarQube.</li>
+            <li>Used AWS services such as: DynamoDB, Redis, Elastic Beanstalk, Lambda.</li>
+            <li>Test-driven development: unit, integration, automation and performance testing.</li>
+        </ul>
         <div class="cv-row-heading">
             <div>Bradleys Foods Ltd. - IT Project Assistant</div>
             <div>August 2017 &ndash; Present</div>
         </div>
         <ul>
-            <li>Analysed company&rsquo;s website in terms of content, speed and SEO.</li>
-            <li>Evaluated and maintained spreadsheets for company&rsquo;s 3000 products.</li>
-            <li>Worked independently and as part of a team.</li>
+            <li>Analysed company’s website in terms of content, speed and SEO.</li>
+            <li>Created and maintain <ExternalLink href="https://cakecraftcentrenorwich.co.uk/">cakecraftcentrenorwich.co.uk</ExternalLink> using classic ASP, showcasing products and services.</li>
+            <li>Evaluated and maintained spreadsheets for company’s 3000 products.</li>
+            <li>Worked independently on assigned tasks within deadlines.</li>
             <li>Developed image compressing software in C# WPF.</li>
-            <li>Able to complete tasks within deadlines.</li>
         </ul>
-        <h3 class="cv-heading">Technical Skills</h3>
-        <ul>
-            <li>Programming Languages: C#, Java, Visual Basic, Pascal.</li>
-            <li>Server-Side Programming: Node.js, PHP, Spring Boot.</li>
-            <li>Front-End Development: HTML5, CSS3, SASS, VueJS.</li>
-            <li>Client-Side Programming: JavaScript, AJAX, npm (Babel, Gulp, Browserify).</li>
-            <li>Databases: DynamoDB, MSSQL, MySQL.</li>
-            <li>Web Administration Tools: phpMyAdmin, cPanel.</li>
-            <li>Amazon Web Services: Elastic Beanstalk, Route 53, RDS, Lambda.</li>
-            <li>Version Control using Git.</li>
-            <li>Clear understanding of Object-Oriented Programming.</li>
-            <li>Experienced in Microsoft Technologies: .NET, WPF.</li>
-        </ul>
+
+        <!-- END SECTION work -->
+        <!-- START SECTION achievements -->
+
         <h3 class="cv-heading">Achievements</h3>
         <div class="cv-row-heading">
-            <div>Extended Project Qualification</div>
+            <div>The "Genesis" Project - Team Project at Yell</div>
             <div></div>
         </div>
         <ul>
-            <li>Achieved A*.</li>
-            <li>Using Microsoft SQL Server, PHP, JavaScript, HTML/CSS.</li>
-            <li>Handled large volume of data in a MSSQL database, which was processed using PHP and visualised on a webpage using JavaScript.</li>
-            <li>Delivered presentation about the process of creating my project and alternatives to my design.</li>
+            <li>Produced analytical dashboard web application as a team of 4.</li>
+            <li>Scheduled and partook in regular meetings with stakeholders.</li>
+            <li>Implemented using modern technologies: VueJS frontend and Spring Boot backend.</li>
+            <li>Delivered several presentations to showcase project.</li>
         </ul>
         <div class="cv-row-heading">
-            <div>Technical Officer/ Software Officer of the Loughborough Technology Society</div>
+            <div>Committee Member of the Loughborough Technology Society - Technical Officer/ Software Officer</div>
             <div></div>
         </div>
         <ul>
             <li>Hosted programming orientated events and workshops, allowing fellow students to participate and learn.</li>
             <li>Mentored attendees on range of topics, including: Docker, Node.js, SASS, JavaScript, HTML/ CSS.</li>
             <li>Worked as a team to create interesting and engaging presentations.</li>
-            <li>Created Society&rsquo;s Website: <ExternalLink href="https://lborotechsoc.github.io/">loughborough.tech</ExternalLink>.</li>
-        </ul>
-        <div class="cv-row-heading">
-            <div>The &ldquo;Genesis&rdquo; Project - Team Project at Yell</div>
-            <div></div>
-        </div>
-        <ul>
-            <li>Analytical dashboard for a new feature by Yell.</li>
-            <li>Regular contact with stakeholders.</li>
-            <li>Used modern technologies: VueJS frontend and Spring Boot backend.</li>
-            <li>Delivered several presentations to showcase project.</li>
+            <li>Created Society&rsquo;s Website: <ExternalLink href="https://lborotechsoc.github.io/">loughborough.tech</ExternalLink>, using Jekyll.</li>
         </ul>
         <div class="cv-row-heading">
             <div>Captain of one of Loughborough&rsquo;s Counter-Strike: Global Offensive (CS:GO) Teams</div>
@@ -140,8 +158,35 @@
         <ul>
             <li>Participated in The NUEL CS:GO Winter Championship 2017.</li>
             <li>Organised members and verified the team was signed up correctly.</li>
-            <li>Made sure team members turned up to scheduled matches.</li>
+            <li>Made sure team members turned up to practice and scheduled matches.</li>
         </ul>
+        <div class="cv-row-heading">
+            <div>6-Week Intensive Mandarin Course in Taiwan (August - September 2019)</div>
+            <div></div>
+        </div>
+        <ul>
+            <li>Completed a 6-week course for learning Mandarin.</li>
+            <li>Participated in daily classes with a group of students.</li>
+        </ul>
+
+        <!-- END SECTION achievements -->
+        <!-- START SECTION technical_skills -->
+
+        <h3 class="cv-heading">Technical Skills</h3>
+        <ul>
+            <li>Programming Languages: C#, Java, Node.js, PHP.</li>
+            <li>Front-End Development: HTML5, CSS3, SASS.</li>
+            <li>Client-Side Programming: JavaScript, VueJS, AJAX, npm.</li>
+            <li>Databases: DynamoDB, MSSQL, MySQL, Redis, Marklogic.</li>
+            <li>Web Administration Tools: phpMyAdmin, cPanel, Jenkins, SonarQube, Redmine, Kibana.</li>
+            <li>Amazon Web Services: Elastic Beanstalk, Route 53, RDS, Lambda.</li>
+            <li>Version Control using Git.</li>
+            <li>Containerisation using Docker.</li>
+        </ul>
+
+        <!-- END SECTION technical_skills -->
+        <!-- START SECTION soft_skills -->
+
         <h3 class="cv-heading">Other Skills</h3>
         <div class="cv-row-heading">
             <div>Time Management and Organisation</div>
@@ -159,6 +204,9 @@
             <li>Voiced opinions and ideas during Technology Society meetings, and made sure presentations and projects were of a high standard, as well as explaining concepts clearly while teaching students topics.</li>
             <li>Expressed ideas and maintained clear communication with fellow team members during the Team Project module in the 2nd year of Computer Science course.</li>
         </ul>
+
+        <!-- END SECTION soft_skills -->
+
     </div>
 </template>
 
