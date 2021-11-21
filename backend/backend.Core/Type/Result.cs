8b6e693190e4bfd1b97dc6a5025ab11629752dac
wjbaker101 <willbaker10198@gmail.com>
2021-11-21 20:24:30 +0000

    Add basic netcore backend API

---
+++ backend/backend.Core/Type/Result.cs
@@ -0,0 +1,18 @@
+﻿namespace backend.Core.Type;
+
+public class Result
+{
+    public string? FailureMessage { get; init; }
+
+    public static Result Failure(string failureMessage)
+    {
+        return new Result
+        {
+            FailureMessage = failureMessage,
+        };
+    }
+}
+
+public sealed class Result<T> : Result
+{
+}
