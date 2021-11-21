a145cb970eb8814d37b3ecd57f2b747e83810e65
wjbaker101 <willbaker10198@gmail.com>
2021-11-21 21:12:46 +0000

    More result logic

--- backend/backend.Core/Type/Result.cs
+++ backend/backend.Core/Type/Result.cs
@@ -2,17 +2,56 @@
 
 public class Result
 {
-    public string? FailureMessage { get; init; }
+    public string? FailureMessage { get; protected set; }
+
+    public bool IsFailure { get; protected set; }
+    public bool IsSuccess => !IsFailure;
+
+    protected Result()
+    {
+        IsFailure = false;
+    }
+
+    protected Result(string failureMessage)
+    {
+        IsFailure = true;
+        FailureMessage = failureMessage;
+    }
+
+    public static Result Success()
+    {
+        return new Result();
+    }
 
     public static Result Failure(string failureMessage)
     {
-        return new Result
-        {
-            FailureMessage = failureMessage,
-        };
+        return new Result(failureMessage);
     }
 }
 
 public sealed class Result<T> : Result
 {
+    public T? Value { get; }
+
+    private Result(T value)
+    {
+        IsFailure = false;
+        Value = value;
+    }
+
+    private Result(string failureMessage)
+    {
+        IsFailure = true;
+        FailureMessage = failureMessage;
+    }
+
+    public Result<T> Of(T value)
+    {
+        return new Result<T>(value);
+    }
+
+    public new static Result<T> Failure(string failureMessage)
+    {
+        return new Result<T>(failureMessage);
+    }
 }
