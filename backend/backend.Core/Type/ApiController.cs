627fc595ab1bd7ae78291a2bb3f90c514645deff
wjbaker101 <willbaker10198@gmail.com>
2021-11-21 22:50:28 +0000

    Add api response wrapper and mapper for controllers

---
+++ backend/backend.Core/Type/ApiController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Core.Type;
+
+public abstract class ApiController : Controller
+{
+    public IActionResult ToApiResponse(Result result)
+    {
+        if (result.IsFailure)
+            return BadRequest();
+        
+        return Ok();
+    }
+
+    public IActionResult ToApiResponse<T>(Result<T> result)
+    {
+        if (result.IsFailure)
+            return BadRequest();
+
+        return Ok(ApiResultResponse<T>.Of(result.Value!));
+    }
+}
---
+++ backend/backend.Core/Type/ApiResponse.cs
@@ -0,0 +1,41 @@
+﻿namespace backend.Core.Type;
+
+public abstract class ApiResponse
+{
+    public DateTimeOffset ResponseAt { get; }
+
+    protected ApiResponse()
+    {
+        ResponseAt = DateTimeOffset.UtcNow;
+    }
+}
+
+public sealed class ApiResultResponse<T> : ApiResponse
+{
+    public T Result { get; }
+
+    private ApiResultResponse(T result)
+    {
+        Result = result;
+    }
+
+    public static ApiResultResponse<T> Of(T result)
+    {
+        return new ApiResultResponse<T>(result);
+    }
+}
+
+public sealed class ApiErrorResponse : ApiResponse
+{
+    public string ErrorMessage { get; }
+
+    private ApiErrorResponse(string errorMessage)
+    {
+        ErrorMessage = errorMessage;
+    }
+
+    public static ApiErrorResponse Of(string errorMessage)
+    {
+        return new ApiErrorResponse(errorMessage);
+    }
+}
