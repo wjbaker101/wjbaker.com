ec75aaf843df1b6c1c3616de8aba2bf1a8697701
wjbaker101 <willbaker10198@gmail.com>
2021-11-23 23:58:01 +0000

    Include the error message on failure

--- backend/backend.Core/Type/ApiController.cs
+++ backend/backend.Core/Type/ApiController.cs
@@ -7,7 +7,7 @@ public abstract class ApiController : Controller
     public IActionResult ToApiResponse(Result result)
     {
         if (result.IsFailure)
-            return BadRequest();
+            return BadRequest(ApiErrorResponse.Of(result.FailureMessage!));
         
         return Ok();
     }
@@ -15,7 +15,7 @@ public abstract class ApiController : Controller
     public IActionResult ToApiResponse<T>(Result<T> result)
     {
         if (result.IsFailure)
-            return BadRequest();
+            return BadRequest(ApiErrorResponse.Of(result.FailureMessage!));
 
         return Ok(ApiResultResponse<T>.Of(result.Value!));
     }
