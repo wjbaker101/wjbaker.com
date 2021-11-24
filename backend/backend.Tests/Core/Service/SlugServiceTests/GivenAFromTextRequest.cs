dbce408993453b75070ce7c7aa22e1e72e5d0e16
wjbaker101 <willbaker10198@gmail.com>
2021-11-24 19:11:37 +0000

    Create tests for the `SlugService` this has highlighted a few bugs which have been fixed in previous commits

---
+++ backend/backend.Tests/Core/Service/SlugServiceTests/GivenAFromTextRequest.cs
@@ -0,0 +1,37 @@
+﻿using backend.Core.Services;
+using backend.Core.Type;
+using NUnit.Framework;
+
+namespace backend.Tests.Core.Service.SlugServiceTests;
+
+[TestFixture("Test Slug", "test-slug")]
+[TestFixture("Test  Slug", "test-slug")]
+[TestFixture("Test   Slug", "test-slug")]
+[TestFixture("Test   Slug", "test-slug")]
+[TestFixture("Test-SLUG", "test-slug")]
+[Parallelizable]
+public sealed class GivenAFromTextRequest
+{
+    private readonly string _before;
+    private readonly string _after;
+    
+    private Result<string> _result = null!;
+
+    public GivenAFromTextRequest(string before, string after)
+    {
+        _before = before;
+        _after = after;
+    }
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        _result = SlugService.FromText(_before);
+    }
+
+    [Test]
+    public void ThenTheCorrectUrlSlugIsReturned()
+    {
+        Assert.That(_result.Value, Is.EqualTo(_after));
+    }
+}
---
+++ backend/backend.Tests/Core/Service/SlugServiceTests/GivenAFromTextRequestWithAnInvalidInput.cs
@@ -0,0 +1,42 @@
+﻿using backend.Core.Services;
+using backend.Core.Type;
+using NUnit.Framework;
+
+namespace backend.Tests.Core.Service.SlugServiceTests;
+
+[TestFixture(null, "Url slug cannot be blank, please try again.")]
+[TestFixture("", "Url slug cannot be blank, please try again.")]
+[TestFixture(" ", "Url slug cannot be blank, please try again.")]
+[TestFixture("---", "Unable to create Url slug.")]
+[Parallelizable]
+public sealed class GivenAFromTextRequestWithAnInvalidInput
+{
+    private readonly string _before;
+    private readonly string _failureMessage;
+
+    private Result<string> _result = null!;
+
+    public GivenAFromTextRequestWithAnInvalidInput(string before, string failureMessage)
+    {
+        _before = before;
+        _failureMessage = failureMessage;
+    }
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        _result = SlugService.FromText(_before);
+    }
+
+    [Test]
+    public void ThenTheResultIsAFailure()
+    {
+        Assert.That(_result.IsFailure, Is.True);
+    }
+
+    [Test]
+    public void ThenTheCorrectFailureMessageIsReturned()
+    {
+        Assert.That(_result.FailureMessage, Is.EqualTo(_failureMessage));
+    }
+}
