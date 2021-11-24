8029240ff9f3a7556560a87e78a0104185ba5d7b
wjbaker101 <willbaker10198@gmail.com>
2021-11-24 20:40:59 +0000

    Create partial implementation of an attribute for endpoints that require authentication

---
+++ backend/backend.Api/Auth/RequiresAuthenticationAttribute.cs
@@ -0,0 +1,45 @@
+﻿using backend.Data.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace backend.Api.Auth;
+
+public sealed class RequiresAuthenticationAttribute : ServiceFilterAttribute
+{
+    public RequiresAuthenticationAttribute() : base(typeof(AttributeImplementation))
+    {
+    }
+
+    private sealed class AttributeImplementation : IActionFilter
+    {
+        private readonly IApiDatabase _apiDatabase;
+
+        public AttributeImplementation(IApiDatabase apiDatabase)
+        {
+            _apiDatabase = apiDatabase;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var authorizationHeader = context.HttpContext.Request.Headers.Authorization.ToString();
+            if (string.IsNullOrWhiteSpace(authorizationHeader))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var split = authorizationHeader.Split(" ");
+            if (split.Length != 2)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var jwt = split[1];
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+    }
+}
