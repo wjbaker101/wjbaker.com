b22f7a151577c3735e63dbb7e0aba57f885dae16
wjbaker101 <willbaker10198@gmail.com>
2021-11-25 21:04:10 +0000

    Add various components used by multiple pages

---
+++ frontend/src/component/Error.component.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="error-component flex">
+        <ErrorIcon class="flex-auto" />
+        <div>
+            <span>{{ message }}</span>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import ErrorIcon from '@/component/icon/ExclamationCircleIcon.component.vue';
+
+export default defineComponent({
+    name: 'ErrorComponent',
+
+    components: {
+        ErrorIcon,
+    },
+
+    props: {
+        message: String,
+    },
+
+    setup() {},
+});
+</script>
+
+<style lang="scss">
+.error-component {
+    padding: 1rem;
+    background-color: theme(negative-light);
+    border: 1px solid theme(negative);
+    border-radius: border-radius();
+    color: theme(white);
+
+    .icon-exclamation-circle {
+        margin: auto 1rem auto 0;
+    }
+}
+</style>
