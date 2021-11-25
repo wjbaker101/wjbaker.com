4e34ab1fb4f16f59bb7a2810673c9bcbaffaa396
wjbaker101 <willbaker10198@gmail.com>
2021-11-25 17:45:11 +0000

    Add generic icon component props

---
+++ frontend/src/component/type/IconProps.ts
@@ -0,0 +1,14 @@
+export const IconProps = {
+    width: {
+        type: Number,
+        default: 15,
+    },
+    height: {
+        type: Number,
+        default: 15,
+    },
+    colour: {
+        type: String,
+        default: 'currentColor',
+    },
+};
