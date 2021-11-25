f1a87d3ae72f31dacf7db47ba6d78cfea0646597
wjbaker101 <willbaker10198@gmail.com>
2021-11-25 17:17:01 +0000

    Copy over LinkComponent, upgrading to Vue 3

---
+++ frontend/src/component/Link.component.vue
@@ -0,0 +1,29 @@
+<template>
+    <a
+        class="link-component"
+        target="_blank"
+        rel="noopener noreferrer"
+        :href="href"
+        :title="title"
+    >
+        <slot />
+    </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'LinkComponent',
+
+    props: {
+        href: String,
+        title: String,
+    },
+
+    setup() {},
+});
+</script>
+
+<style lang="scss">
+</style>
