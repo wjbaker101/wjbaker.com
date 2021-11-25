03c351daa69a779c0c550b2402e25cfc3f0f3d4c
wjbaker101 <willbaker10198@gmail.com>
2021-11-25 18:12:23 +0000

    Add aside and styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,38 @@
 <template>
+    <AsideComponent />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import AsideComponent from '@/component/layout/Aside.component.vue';
+
 export default defineComponent({
     name: 'App',
+
+    components: {
+        AsideComponent,
+    },
 
     setup() {},
 });
 </script>
 
 <style lang="scss">
+@import './style/main';
+
+#app {
+    height: 100%;
+
+    @media screen and (max-width: breakpoint()) {
+        height: auto;
+        display: block;
+    }
+
+    .aside-placeholder {
+        width: 25%;
+        min-width: 300px;
+        max-width: 430px;
+    }
+}
 </style>
