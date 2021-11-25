d33117c084f342442bbee1c79578048a4b24c7ef
wjbaker101 <willbaker10198@gmail.com>
2021-11-25 17:44:59 +0000

    Swap svg files to components so that there is no need for the svg loader

---
+++ frontend/src/component/icon/BriefcaseIcon.component.vue
@@ -0,0 +1,20 @@
+<template>
+    <svg :width="width" :height="height" aria-hidden="true" focusable="false" class="svg-icon icon-briefcase" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path :fill="colour" d="M320 336c0 8.84-7.16 16-16 16h-96c-8.84 0-16-7.16-16-16v-48H0v144c0 25.6 22.4 48 48 48h416c25.6 0 48-22.4 48-48V288H320v48zm144-208h-80V80c0-25.6-22.4-48-48-48H176c-25.6 0-48 22.4-48 48v48H48c-25.6 0-48 22.4-48 48v80h512v-80c0-25.6-22.4-48-48-48zm-144 0H192V96h128v32z"></path></svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import { IconProps } from '@/component/type/IconProps';
+
+export default defineComponent({
+    name: 'BriefcaseIcon',
+
+    props: IconProps,
+
+    setup() {},
+});
+</script>
+
+<style lang="scss">
+</style>
