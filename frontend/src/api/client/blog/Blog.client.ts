6ea3e45bb51b1d532ae6d53b52026891f4096120
wjbaker101 <willbaker10198@gmail.com>
2021-11-26 19:18:39 +0000

    Extend api client for blog post retrieval

--- frontend/src/api/client/blog/Blog.client.ts
+++ frontend/src/api/client/blog/Blog.client.ts
@@ -2,6 +2,7 @@ import { apiClient, handleError } from '@/api/ApiClient';
 
 import { ApiResultResponse } from '@/api/type/ApiResponse.type';
 import { SearchBlogResponse } from '@/api/client/blog/type/SearchBlog.type';
+import { GetBlogPostByResponse } from '@/api/client/blog/type/GetBlogPostBy.type';
 
 class BlogClient {
 
@@ -12,6 +13,28 @@ class BlogClient {
                     page,
                 },
             });
+
+            return response.data.result;
+        }
+        catch (error) {
+            return handleError(error);
+        }
+    }
+
+    public async getBlogPostByUrlSlug(urlSlug: string): Promise<GetBlogPostByResponse | Error> {
+        try {
+            const response = await apiClient.get<ApiResultResponse<GetBlogPostByResponse>>(`/blog/post/${urlSlug}`);
+
+            return response.data.result;
+        }
+        catch (error) {
+            return handleError(error);
+        }
+    }
+
+    public async getBlogPostByReference(reference: string): Promise<GetBlogPostByResponse | Error> {
+        try {
+            const response = await apiClient.get<ApiResultResponse<GetBlogPostByResponse>>(`/blog/post/${reference}`);
 
             return response.data.result;
         }
---
+++ frontend/src/api/client/blog/type/GetBlogPostBy.type.ts
@@ -0,0 +1,8 @@
+export interface GetBlogPostByResponse {
+    reference: string;
+    title: string;
+    urlSlug: string;
+    postedAt: string;
+    summary: string;
+    content: string;
+}
