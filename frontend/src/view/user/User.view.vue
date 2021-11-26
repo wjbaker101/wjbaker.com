3558b8d0a73a256e33738dd62faaad7c62be1214
wjbaker101 <willbaker10198@gmail.com>
2021-11-26 21:14:19 +0000

    Add details to user view

--- frontend/src/view/user/User.view.vue
+++ frontend/src/view/user/User.view.vue
@@ -1,14 +1,20 @@
 <template>
-    <PageContentComponent class="user-view">
+    <PageContentComponent class="user-view" v-if="user !== null">
         <PageTitleComponent title="Your User" />
         <p>
             <strong>Username:</strong> {{ user.username }}
+        </p>
+        <p>
+            <strong>Created:</strong> {{ displayCreatedAt }} ({{ displayCreatedAtDifference }})
+        </p>
+        <p>
+            <strong>User Type:</strong> {{ displayUserType }}
         </p>
     </PageContentComponent>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue';
+import { computed, defineComponent, onMounted, ref } from 'vue';
 import dayjs from 'dayjs';
 
 import PageContentComponent from '@/component/layout/PageContent.component.vue';
@@ -17,7 +23,7 @@ import PageTitleComponent from '@/component/PageTitle.component.vue';
 import { userClient } from '@/api/client/user/User.client';
 import { userService } from '@/service/User.service';
 
-import { User } from '@/model/User.model';
+import { User, UserType } from '@/model/User.model';
 
 export default defineComponent({
     name: 'UserView',
@@ -31,6 +37,22 @@ export default defineComponent({
         const authDetails = userService.getAuthDetails();
 
         const user = ref<User | null>(null);
+
+        const displayCreatedAt = computed<string>(() => user.value?.createdAt.format('DD/MM/YYYY') ?? '');
+        const displayCreatedAtDifference = computed<string>(() => user.value?.createdAt.fromNow() ?? '');
+
+        const displayUserType = computed<string>(() => {
+            switch (user.value?.type) {
+                case UserType.Unknown:
+                    return 'Unknown';
+                case UserType.Admin:
+                    return 'Admin';
+                case UserType.Basic:
+                    return 'Basic';
+                default:
+                    return 'Unknown';
+            }
+        });
 
         onMounted(async () => {
             if (authDetails.value === null)
@@ -50,7 +72,10 @@ export default defineComponent({
         });
 
         return {
-
+            user,
+            displayCreatedAt,
+            displayCreatedAtDifference,
+            displayUserType,
         }
     },
 });
