8d75aed7d60c1b1f98a5f3b0178f7c6624dc8aa4
wjbaker101 <willbaker10198@gmail.com>
2021-11-26 20:54:52 +0000

    Add user view

--- frontend/src/router/app.router.ts
+++ frontend/src/router/app.router.ts
@@ -60,6 +60,13 @@ const routes: Array<RouteRecordRaw> = [
         component: () => import(/* webpackChunkName: "blog" */ '@/view/blog-post/BlogPost.view.vue'),
     },
     {
+        path: '/user',
+        meta: {
+            title: 'Your User',
+        },
+        component: () => import(/* webpackChunkName: "blog" */ '@/view/user/User.view.vue'),
+    },
+    {
         path: '/user/login',
         meta: {
             title: 'Log In',
---
+++ frontend/src/view/user/User.view.vue
@@ -0,0 +1,60 @@
+<template>
+    <PageContentComponent class="user-view">
+        <PageTitleComponent title="Your User" />
+        <p>
+            <strong>Username:</strong> {{ user.username }}
+        </p>
+    </PageContentComponent>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import dayjs from 'dayjs';
+
+import PageContentComponent from '@/component/layout/PageContent.component.vue';
+import PageTitleComponent from '@/component/PageTitle.component.vue';
+
+import { userClient } from '@/api/client/user/User.client';
+import { userService } from '@/service/User.service';
+
+import { User } from '@/model/User.model';
+
+export default defineComponent({
+    name: 'UserView',
+
+    components: {
+        PageContentComponent,
+        PageTitleComponent,
+    },
+
+    setup() {
+        const userDetails = userService.getUser();
+
+        const user = ref<User | null>(null);
+
+        onMounted(async () => {
+            if (userDetails === null)
+                return;
+
+            const result = await userClient.getUserByReference(userDetails.reference);
+            if (result instanceof Error) {
+                return;
+            }
+
+            user.value = {
+                reference: result.reference,
+                username: result.username,
+                createdAt: dayjs(result.createdAt),
+                type: result.type,
+            };
+        });
+
+        return {
+
+        }
+    },
+});
+</script>
+
+<style lang="scss">
+</style>
