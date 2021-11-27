79f2c34176e1555fb3f44a23c4bb8fd64ed44d31
wjbaker101 <willbaker10198@gmail.com>
2021-11-27 19:58:00 +0000

    Add create and update to projects api client

--- frontend/src/api/client/projects/Project.client.ts
+++ frontend/src/api/client/projects/Project.client.ts
@@ -3,6 +3,8 @@ import { apiClient, handleError } from '@/api/ApiClient';
 import { ApiResultResponse } from '@/api/type/ApiResponse.type';
 import { SearchProjectsResponse } from '@/api/client/projects/type/SearchProjects.type';
 import { GetProjectByResponse } from '@/api/client/projects/type/GetProjectBy.type';
+import { UpdateProjectRequest, UpdateProjectResponse } from '@/api/client/projects/type/UpdateProject.type';
+import { CreateProjectRequest, CreateProjectResponse } from '@/api/client/projects/type/CreateProject.type';
 
 class ProjectClient {
 
@@ -35,6 +37,28 @@ class ProjectClient {
     public async getProjectByReference(reference: string): Promise<GetProjectByResponse | Error> {
         try {
             const response = await apiClient.get<ApiResultResponse<GetProjectByResponse>>(`/project/${reference}`);
+
+            return response.data.result;
+        }
+        catch (error) {
+            return handleError(error);
+        }
+    }
+
+    public async createProject(reference: string, request: CreateProjectRequest): Promise<CreateProjectResponse | Error> {
+        try {
+            const response = await apiClient.put<ApiResultResponse<CreateProjectResponse>>(`/project/${reference}`, request);
+
+            return response.data.result;
+        }
+        catch (error) {
+            return handleError(error);
+        }
+    }
+
+    public async updateProject(reference: string, request: UpdateProjectRequest): Promise<UpdateProjectResponse | Error> {
+        try {
+            const response = await apiClient.put<ApiResultResponse<UpdateProjectResponse>>(`/project/${reference}`, request);
 
             return response.data.result;
         }
---
+++ frontend/src/api/client/projects/type/CreateProject.type.ts
@@ -0,0 +1,27 @@
+export interface CreateProjectRequest {
+    title: string;
+    urlSlug: string;
+    startedAt: string;
+    summary: string;
+    description: string | null;
+    sourceCodeUrl: string | null;
+    previewImageUrl: string | null;
+    displayOrder: number;
+    viewUrl: string | null;
+    tags: Array<string>;
+}
+
+export interface CreateProjectResponse {
+    reference: string;
+    title: string;
+    urlSlug: string;
+    startedAt: string;
+    summary: string;
+    description: string | null;
+    sourceCodeUrl: string | null;
+    previewImageUrl: string | null;
+    displayOrder: number;
+    createdAt: string;
+    viewUrl: string | null;
+    tags: Array<string>;
+}
