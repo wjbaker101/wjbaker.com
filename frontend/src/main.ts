b6b0a52f002784c25afb7475a4fd927f8cf61d37
wjbaker101 <willbaker10198@gmail.com>
2021-11-28 15:28:43 +0000

    Add logic for today's date

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,9 +3,11 @@ import dayjs from 'dayjs';
 
 import advancedFormat from 'dayjs/plugin/advancedFormat';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import isToday from 'dayjs/plugin/isToday';
 
 dayjs.extend(advancedFormat);
 dayjs.extend(relativeTime);
+dayjs.extend(isToday);
 
 import Particles from 'particles.vue3';
 import { appRouter } from '@/router/app.router';
--- frontend/src/view/about/component/Timeline.component.vue
+++ frontend/src/view/about/component/Timeline.component.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="timeline-component flex flex-vertical">
-        <div :key="`timeline-item-${index}`" v-for="(item, index) in timeline.items" class="item">
+        <div :key="`timeline-item-${index}`" v-for="(item, index) in displayItems" class="item">
             <h6>
                 <div class="node" :class="{ 'has-icon': item.type !== TimelineType.None }">
                     <Component :is="mapIcon(item.type)" />
                 </div>
-                <span>{{ item.date.format('MMMM YYYY') }}</span>
-                <span v-if="item.endDate !== null"> &mdash; {{ item.endDate.format('MMMM YYYY') }}</span>
+                <span>{{ mapDate(item.date) }}</span>
+                <span v-if="item.endDate !== null"> &mdash; {{ mapEndDate(item.endDate) }}</span>
             </h6>
             <p>{{ item.summary }}</p>
         </div>
@@ -14,12 +14,13 @@
 </template>
 
 <script lang="ts">
-import { Component, defineComponent, PropType } from 'vue';
+import { Component, computed, defineComponent, PropType } from 'vue';
+import dayjs, { Dayjs } from 'dayjs';
 
 import EducationIcon from '@/component/icon/GraduationCapIcon.component.vue';
 import WorkIcon from '@/component/icon/BriefcaseIcon.component.vue';
 
-import { Timeline, TimelineType } from '@/model/Timeline.model';
+import { Timeline, TimelineItem, TimelineType } from '@/model/Timeline.model';
 
 export default defineComponent({
     name: 'TimelineComponent',
@@ -31,9 +32,19 @@ export default defineComponent({
         },
     },
 
-    setup() {
+    setup(props) {
+        const today: TimelineItem = {
+            date: dayjs(),
+            endDate: null,
+            type: TimelineType.None,
+            summary: 'Thank you for visiting my website :)',
+        };
+
+        const displayItems = computed<Array<TimelineItem>>(() => [ today ].concat(props.timeline.items));
+
         return {
             TimelineType,
+            displayItems,
 
             mapIcon(type: TimelineType): Component | null {
                 switch (type) {
@@ -43,6 +54,20 @@ export default defineComponent({
                     default:
                         return null;
                 }
+            },
+
+            mapDate(date: Dayjs): string {
+                if (date.isToday()) 
+                    return 'Today!';
+                
+                return date.format('MMMM YYYY');
+            },
+
+            mapEndDate(endDate: Dayjs): string {
+                if (endDate.isToday()) 
+                    return 'Present';
+                
+                return endDate.format('MMMM YYYY');
             },
         }
     },
