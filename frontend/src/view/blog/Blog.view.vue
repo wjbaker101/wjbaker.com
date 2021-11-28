59349d1b1c02276c00c7b1050a4e3413ecdc1155
wjbaker101 <willbaker10198@gmail.com>
2021-11-28 13:10:13 +0000

    Add button to create a new blog post

--- frontend/src/view/blog/Blog.view.vue
+++ frontend/src/view/blog/Blog.view.vue
@@ -9,6 +9,13 @@
             message="Unable to load blog; try refreshing the page."
         />
         <div v-else>
+            <PageActionsBarComponent v-if="isAdmin">
+                <template v-slot:right>
+                    <router-link to="/blog/post/edit">
+                        <ButtonComponent><AddIcon /></ButtonComponent>
+                    </router-link>
+                </template>
+            </PageActionsBarComponent>
             <section class="blog-posts">
                 <BlogPostComponent
                     :key="blogPost"
@@ -48,14 +55,18 @@ import PageContentComponent from '@/component/layout/PageContent.component.vue';
 import PageTitleComponent from '@/component/PageTitle.component.vue';
 import PageActionsBarComponent from '@/component/PageActionsBar.component.vue';
 import LoadingComponent from '@/component/Loading.component.vue';
+import ButtonComponent from '@/component/Button.component.vue';
 import ErrorComponent from '@/component/Error.component.vue';
 import BlogPostComponent from '@/view/blog/component/BlogPost.component.vue';
 import ArrowLeftIconComponent from '@/component/icon/ArrowLeftIcon.component.vue';
 import ArrowRightIconComponent from '@/component/icon/ArrowRightIcon.component.vue';
+import AddIcon from '@/component/icon/PlusIcon.component.vue';
 
 import { blogClient } from '@/api/client/blog/Blog.client';
+import { userService } from '@/service/user/User.service';
 
 import { BlogPost } from '@/model/BlogPost.model';
+import { UserType } from '@/model/User.model';
 
 export default defineComponent({
     name: 'BlogView',
@@ -64,15 +75,20 @@ export default defineComponent({
         PageContentComponent,
         PageTitleComponent,
         LoadingComponent,
+        ButtonComponent,
         ErrorComponent,
         BlogPostComponent,
         PageActionsBarComponent,
         ArrowLeftIconComponent,
         ArrowRightIconComponent,
+        AddIcon,
     },
 
     setup() {
         const route = useRoute();
+
+        const authDetails = userService.getAuthDetails();
+        const isAdmin = computed<boolean>(() => authDetails.value?.user.type === UserType.Admin);
         
         const blogPosts = ref<Array<BlogPost>>([]);
         const isLoading = ref<boolean>(false);
@@ -140,6 +156,7 @@ export default defineComponent({
         });
 
         return {
+            isAdmin,
             blogPosts,
             isLoading,
             isError,
