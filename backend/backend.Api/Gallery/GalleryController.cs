26021ce2fb507b50e00ca3ee3d69d2a5c01ebdea
wjbaker101 <willbaker10198@gmail.com>
2021-11-29 19:18:32 +0000

    Add gallery controller for handling everything photo related

---
+++ backend/backend.Api/Gallery/GalleryController.cs
@@ -0,0 +1,15 @@
+﻿using backend.Core.Type;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Api.Gallery;
+
+[Route("api/gallery")]
+public sealed class GalleryController : ApiController
+{
+    private readonly IGalleryService _galleryService;
+
+    public GalleryController(IGalleryService galleryService)
+    {
+        _galleryService = galleryService;
+    }
+}
---
+++ backend/backend.Api/Gallery/GalleryService.cs
@@ -0,0 +1,17 @@
+﻿using backend.Core.Client.Flickr;
+
+namespace backend.Api.Gallery;
+
+public interface IGalleryService
+{
+}
+
+public sealed class GalleryService : IGalleryService
+{
+    private readonly IFlickrClient _flickrClient;
+
+    public GalleryService(IFlickrClient flickrClient)
+    {
+        _flickrClient = flickrClient;
+    }
+}
