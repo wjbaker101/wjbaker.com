17125cd82310b4360de503deb2c5d8b6e1fe79b0
wjbaker101 <willbaker10198@gmail.com>
2023-02-12 21:26:45 +0000

    Move and rename files

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,11 @@
 import { createApp } from 'vue';
 import Particles from 'particles.vue3';
 
+import App from '@/App.vue';
 import { appRouter } from '@/router/app.router';
 
-import App from '@/App.vue';
-
-import '@/setup/dayjs.setup';
-import '@/registerServiceWorker';
+import '@/setup/dayjs';
+import '@/setup/service-worker';
 
 createApp(App)
     .use(Particles)
